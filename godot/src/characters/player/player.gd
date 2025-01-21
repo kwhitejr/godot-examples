f68e713ba7863ec189390692f16cff3b95247759
Kevin White <kwhitejr@gmail.com>
2025-01-20 21:11:18 -0800

    Mouse click movement

--- godot/src/characters/player/player.gd
+++ godot/src/characters/player/player.gd
@@ -3,27 +3,50 @@ extends CharacterBody2D
 
 @export var speed : float = 200.0
 
-signal player_idle(primary_direction : Vector2)
-signal player_walk(primary_direction : Vector2)
+signal player_idle(normalized : Vector2)
+signal player_walk(normalized : Vector2)
 
-var primary_direction : Vector2
+var normalized_direction := Vector2()
+var click_position := position
+var target_position := Vector2()
+
+var MIN_DISTANCE_PIXELS: float = 3.0
+
+#func _ready() -> void:
+	
 
 func _physics_process(delta: float) -> void:
+	handle_directional_movement()
+	handle_click_movement()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func handle_directional_movement() -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if direction:
 		velocity = direction * speed
-		primary_direction = direction.normalized().snapped(Vector2.ONE)
-		player_walk.emit(primary_direction)
+		normalized_direction = direction.normalized().snapped(Vector2.ONE)
+		player_walk.emit(normalized_direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.y = move_toward(velocity.y, 0, speed)
-		player_idle.emit(primary_direction)
+		player_idle.emit(normalized_direction)
 
 	move_and_slide()
+	
+func handle_click_movement() -> void:
+	if Input.is_action_just_pressed("move_to_click"):
+		click_position = get_global_mouse_position()
+	
+	if position.distance_to(click_position) > MIN_DISTANCE_PIXELS:
+		target_position = (click_position - position).normalized()
+		velocity = target_position * speed
+		player_walk.emit(target_position.snapped(Vector2.ONE))
+	else:
+		player_idle.emit(target_position)
+		
+	
+	move_and_slide()
+	
 
 func collect_egg(egg : Egg) -> void:
 	Events.emit_signal("collect_egg")
